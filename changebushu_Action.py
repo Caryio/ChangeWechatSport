e02411508b344d13943142d5f81ad22dea00d506
CaryioCAT <41476501+Caryio@users.noreply.github.com>
2022-07-21 13:56:38 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -107,5 +107,6 @@ if __name__ == "__main__":
     user = os.environ['USER_PHONE']
     password = os.environ['USER_PWD']
     step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
+    # step = os.environ['STEP']
     # step = str(randint(10123, 12302)) 
     main()
